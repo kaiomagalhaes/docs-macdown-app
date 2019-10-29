01aa6972ed875f7b0bb695bbb62d84bacd134ec7
Pablo Resnizky <presnizky@gmail.com>
2019-10-29 00:59:36 -0300

    Add jest testing and initial tests for existing components

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  // The root of your source code, typically /src
+  // `<rootDir>` is a token Jest substitutes
+  roots: ["<rootDir>/tests"],
+
+  // Jest transformations -- this adds support for TypeScript
+  // using ts-jest
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+
+  // Runs special logic, such as cleaning up components
+  // when using React Testing Library and adds special
+  // extended assertions to Jest
+  setupFilesAfterEnv: [
+    "@testing-library/jest-dom/extend-expect"
+  ],
+
+  // Test spec file resolution pattern
+  // Matches parent folder `tests` and filename
+  // should contain `test` or `spec`.
+  testRegex: "(/tests/.*|(\\.|/)(test|spec))\\.tsx?$",
+
+  // Module file extensions for importing
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+};
