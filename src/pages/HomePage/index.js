4ac192183896f6468b68e39c4dd7003761d318c5
Victor <vcarvalho0402@gmail.com>
2019-12-28 14:22:53 -0300

    Replace materialUI makeStyles to css modules

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -4,52 +4,16 @@ import Skeleton from '@material-ui/lab/Skeleton';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import {makeStyles} from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Navbar from "../../components/Navbar";
 import {listFolders} from "../../reducers/folders.reducer";
 import connect from "react-redux/es/connect/connect";
-
-const useStyles = makeStyles(theme => ({
-  icon: {
-    marginRight: theme.spacing(2),
-  },
-  heroContent: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6),
-  },
-  heroButtons: {
-    marginTop: theme.spacing(4),
-  },
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-  },
-  card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  cardMedia: {
-    paddingTop: '56.25%', // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
-  },
-}));
-
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+import styles from './HomePage.module.scss';
 
 const HomePage = (props) => {
   const [isLoadingFolders, setIsLoadingFolders] = useState(true);
-  const classes = useStyles();
 
   useEffect(() => {
     const loadFolders = async () => {
@@ -65,7 +29,7 @@ const HomePage = (props) => {
       <Navbar color='primary'/>
 
       <main>
-        <div className={classes.heroContent}>
+        <div className={styles['hero-content']}>
           <Container maxWidth="sm">
             <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
               Codelitt Docs
@@ -75,7 +39,7 @@ const HomePage = (props) => {
               dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
               ex ea commodo consequat.
             </Typography>
-            <div className={classes.heroButtons}>
+            <div className={styles['hero-buttons']}>
               <Grid container spacing={2} justify="center">
                 <Grid item>
                   <Button variant="contained" color="primary">
@@ -92,7 +56,7 @@ const HomePage = (props) => {
           </Container>
         </div>
 
-        <Container className={classes.cardGrid} maxWidth="md">
+        <Container className={styles['card-grid']} maxWidth="md">
           {/* End hero unit */}
           <Grid container spacing={4}>
             {isLoadingFolders && [1, 2, 3, 4, 5, 6, 7, 8, 9].map(card => (
@@ -105,8 +69,8 @@ const HomePage = (props) => {
 
             {!isLoadingFolders && props.folders.map(folder => (
               <Grid item key={folder.id} xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardContent className={classes.cardContent}>
+                <Card className={styles.card}>
+                  <CardContent className={styles['card-content']}>
                     <Typography gutterBottom variant="h5" component="h2">
                       {folder.name}
                     </Typography>
